009769e0304958a19ec1c89caa90c28ba6831573
kola9810 <nikosad2222@gmail.com>
2022-03-08 13:53:50 +0400

    add function 'tiktok'

--- Bot.py
+++ Bot.py
@@ -2,13 +2,8 @@ import random,time
 from aiogram import *
 from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
 
-
 bot = Bot(token="5114052117:AAH2ZINSWNf-9YSP8p3Bzh-Mk3gKEMppbq4")
 dp = Dispatcher(bot)
-
-@dp.message_handler(text=['.','.'])
-async def messages(message: types.Message):
-    await message.delete()
 
 @dp.message_handler(content_types=["new_chat_members"])
 async def newUser(message: types.Message):
@@ -34,26 +29,6 @@ async def newUser(message: types.Message):
         
     if welcome == 5:
         await message.answer("Приветики\nя Коля\nмне 10 лет\nнадеюсь в этом хавусе вам будет комфортно:)❤️❤️")
-        
-
-
-playmark = ReplyKeyboardMarkup(resize_keyboard=True)
-abobtmi = KeyboardButton("в AdoptMe")
-mm2 = KeyboardButton('Murder mustory2')
-ikea = KeyboardButton('В икею')
-
-exitpl = ReplyKeyboardMarkup(resize_keyboard=True,row_width=1).add(KeyboardButton('.'))
-
-playmark.add(abobtmi,mm2,ikea)
-
-@dp.message_handler(commands=['play','играть','play@MikolaCatBot'])
-async def play(message: types.Message):
-    await bot.send_message(message.from_user.id,reply_markup=playmark)
-    
-@dp.message_handler(text=['В икею','Murder mustory2','в AdoptMe'])
-async def exit(message: types.Message):
-    await bot.send_message(message.from_user.id,reply_markup=exitpl)
-    await message.delete()
 
 if __name__ == "__main__":
     executor.start_polling(dp,skip_updates=True)
